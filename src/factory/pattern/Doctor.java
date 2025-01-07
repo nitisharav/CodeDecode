d2c2e56cf048eb1a1bb29d3088324b99c2d72b16
Nitish Kumar <nitisharav@gmail.com>
2025-01-07 15:35:54 +0530

    Factory pattern code decode

---
+++ src/factory/pattern/Doctor.java
@@ -0,0 +1,11 @@
+package factory.pattern;
+
+public class Doctor implements Profession {
+
+	@Override
+	public void print() {
+		System.out.println("In print of Doctor class");
+
+	}
+
+}
---
+++ src/factory/pattern/FactoryPatternMainClass.java
@@ -0,0 +1,12 @@
+package factory.pattern;
+
+public class FactoryPatternMainClass {
+
+	public static void main(String[] args) {
+		ProfessionFactory professionFactory = new ProfessionFactory();
+		Profession doc = professionFactory.getProfession("Doctor");
+		doc.print();
+
+	}
+
+}
